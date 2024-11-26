794cad260cd0880b759f36891649785873c63a14
ijasahamedstr <ijasahamed.str@gmail.com>
2024-11-26 18:11:46 +0200

    commit

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,6 @@
 import CompanyProfile from "./CompanyProfile";
 import IconBar from "./IconBar";
 import FadeCarousel from "./Slider";
-import VisionandMission from "./Vistion_and_Misstion";
 
 export default function Home() {
     return (
@@ -9,7 +8,6 @@ export default function Home() {
       <FadeCarousel/>
       <IconBar/>
       <CompanyProfile/>
-      <VisionandMission/>
       </>
     );
   }
