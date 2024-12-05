e92936011110ad1b422ea60ae71a4825269437c9
ijasahamedstr <ijasahamed.str@gmail.com>
2024-12-05 15:16:51 +0200

    commit

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -2,6 +2,7 @@
 export default function Aboutus() {
     return (
       <>
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
       <h1 style={{textAlign:'center'}}>مــواهــب
       أولادنا و بناتنا</h1>
       </>
